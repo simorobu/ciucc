150a241d984250dc3e8263c86e137c0b6a02f59f
Simona Robustella <50445754+simorobu@users.noreply.github.com>
2024-01-31 10:30:41 +0100

    Update script.js

--- script.js
+++ script.js
@@ -34,7 +34,7 @@ function showMessage(response) {
 
     // Update text content, show message, and change image source to "dance.gif"
     const yesMessage = document.getElementById("question");
-    yesMessage.textContent = "LESGOOO see you on the 14th babygirl😘😘";
+    yesMessage.textContent = "LESGOOO ci vediamo il 14 baby ciucc😘😘";
     yesMessage.style.display = "block";
     yesMessage.style.fontStyle = "normal";
     document.getElementsByClassName("image")[0].src = "images/dance.gif";
